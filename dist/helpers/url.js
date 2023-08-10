15c3c9bf4e55e7f585a3cd33b4686816a0d528bb
Doniyor Doniyorov <ddonierov96@gmail.com>
2023-08-10 14:49:42 +0500

    change

--- dist/helpers/url.js
+++ dist/helpers/url.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const url = "http://localhost:9000";
+const url = "https://product-microservice-idr8.onrender.com";
 exports.default = url;
 //# sourceMappingURL=url.js.map
--- dist/index.js
+++ dist/index.js
@@ -8,12 +8,10 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const product_1 = __importDefault(require("./routes/product"));
 const path_1 = __importDefault(require("path"));
-const auth_1 = __importDefault(require("./middleware/auth"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.static(path_1.default.join(__dirname, '..', 'public', 'images')));
 app.use(express_1.default.json());
-app.use(auth_1.default);
 app.use("/api/product", product_1.default);
 function Run() {
     const PORT = process.env.PORT || 9000;
